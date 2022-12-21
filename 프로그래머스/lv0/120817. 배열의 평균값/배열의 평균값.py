653458978520eee683f868f18cfd8d182f9929b5
l52w <62008219+l52w@users.noreply.github.com>
2022-12-22 08:52:59 +0900

    Update 배열의 평균값.py

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
@@ -2,3 +2,7 @@ def solution(numbers):
     nums_sum = sum(numbers)
     nums_len = len(numbers)
     return nums_sum / nums_len
+
+# 다른 사람의 풀이
+# def solution(numbers):
+#     return sum(numbers) / len(numbers)
