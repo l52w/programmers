77917341a6fe3c149df0e95b65a84de2ef727fbb
l52w <62008219+l52w@users.noreply.github.com>
2022-12-21 09:58:38 +0900

    Update 평균 구하기.py

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
@@ -3,3 +3,12 @@ def solution(arr):
     sum_list = sum(arr)
     answer = sum_list/len(arr)
     return answer
+
+# 다른 사람의 풀이
+# def solution(num):
+#     answer = ''
+#     if num%2==0:
+#         return("Even")
+#     else :
+#         return("Odd")
+#     return answer
