b1eeedf770bbe5af2f3fb71a2d5fe2a21779afd8
l52w <62008219+l52w@users.noreply.github.com>
2022-12-23 10:35:16 +0900

    [level 0] Title: 분수의 덧셈, Time: 1.16 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,18 @@
+def solution(denum1, num1, denum2, num2):
+    # 1. 두 분수의 합 계산
+    m = num1 * num2
+    j = denum1 * num2 + denum2 * num1
+    
+    # 2. 최대공약수 계산
+    start = max(m, j)
+    x = 1
+    
+    for num in range(start, 0, -1):
+        if m % num == 0 and j % num == 0:
+            x = num
+            break
+    
+
+    # 3. gcd 로 나눈 값을 answer에 담기
+    answer = [j / x, m / x]
+    return answer
