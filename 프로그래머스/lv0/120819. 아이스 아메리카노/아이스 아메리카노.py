27e5c87a457d1c755cdcbd6efe9beb92a3eecc5a
l52w <62008219+l52w@users.noreply.github.com>
2022-12-26 09:27:11 +0900

    Update 아이스 아메리카노.py

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -4,3 +4,8 @@ def solution(money):
     answer.insert(0,n)
     answer.insert(1,money-5500*n)
     return answer
+
+# 다른 사람의 풀이
+# def solution(money):
+#     answer = [money // 5500, money % 5500]
+#     return answer
