a4415ed0f27163843cdcf183b7d0c862d35f8953
l52w <62008219+l52w@users.noreply.github.com>
2022-12-29 13:58:43 +0900

    Update 머쓱이보다 키 큰 사람.py

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -4,3 +4,9 @@ def solution(array, height):
         if array[i] > height :
             answer += 1
     return answer
+
+# 다른 사람의 풀이
+# def solution(array, height):
+#     array.append(height)
+#     array.sort(reverse=True)
+#     return array.index(height)
