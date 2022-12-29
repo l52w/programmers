39576491d8b5255083920671394f0441b1c0457b
l52w <62008219+l52w@users.noreply.github.com>
2022-12-29 14:05:17 +0900

    Update 점의 위치 구하기.py

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -9,3 +9,14 @@ def solution(dot):
     else :
         answer = 4
     return answer
+
+# 다른 사람의 풀이
+# def solution(dot):
+#     if dot[0] > 0:
+#         if dot[1] > 0:
+#             return 1
+#         return 4
+#     else:
+#         if dot[1] > 0:
+#             return 2
+#         return 3
