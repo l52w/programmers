a2a8bd0d5da483670bb885e05abc3b2152683d3a
l52w <62008219+l52w@users.noreply.github.com>
2023-01-03 21:25:06 +0900

    Update 옷가게 할인 받기.py

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -7,3 +7,13 @@ def solution(price) :
         return int(price*0.95)
     else :
         return price
+    
+#     다른 사람의 풀이
+#     def solution(price):
+#     if price>=500000:
+#         price = price *0.8
+#     elif price>=300000:
+#         price = price *0.9
+#     elif price>=100000:
+#         price = price * 0.95
+#     return int(price)
