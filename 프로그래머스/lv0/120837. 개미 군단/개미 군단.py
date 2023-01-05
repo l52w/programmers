9cb820876aaf7cfdcb0fb32755d08f8665006c2c
l52w <62008219+l52w@users.noreply.github.com>
2023-01-05 12:39:51 +0900

    Update 개미 군단.py

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
@@ -3,3 +3,7 @@ def solution(hp):
     b = (hp - (a*5)) // 3
     c = (hp - (a*5) - (b*3)) // 1
     return a+b+c
+
+# 다른 사람의 풀이
+# def solution(hp):    
+#     return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
