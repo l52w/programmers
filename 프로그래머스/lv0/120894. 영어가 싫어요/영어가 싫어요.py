9ca147f819b93f094ddc7f484feabf5b97aed957
l52w <62008219+l52w@users.noreply.github.com>
2023-06-06 16:49:07 +0900

    Update 영어가 싫어요.py

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -4,3 +4,15 @@ def solution(numbers):
     for index, num in enumerate(num) :
         numbers = numbers.replace(num, str(index))
     return int(numbers)
+
+# for index, num in enumerate(num) ==> 0 'zero', 1 'one', ...
+# numbers = numbers.replace(num, str(index)) ==> 'zero'->'0' , 'one'->'1', ...
+# return int(numbers) ==> 0,1, ...
+
+# enumerate내장함수
+# for i, letter in enumberate(['A','B','C'], start=1) :
+#     print(i, letter)
+#
+# 1 A
+# 2 B
+# 3 C
