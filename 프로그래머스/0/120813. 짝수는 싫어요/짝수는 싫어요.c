842354cbc409b3128293fdeb9315d20cb7319f1b
l52w <62008219+l52w@users.noreply.github.com>
2024-01-20 00:30:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.c
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.c
@@ -5,7 +5,7 @@
 int* solution(int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int a = 0;
-    int* answer = (int*)malloc(200);
+    int* answer = (int*)malloc(((n/2)+1) * sizeof(int));
     for(int i=1; i<=n; i+=2){
         answer[a] = i;
         a++;
