137c2c0ca64d569fdfdb2f83370ecd84ebe8cd52
l52w <62008219+l52w@users.noreply.github.com>
2024-02-15 10:54:00 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len, int k) {
+    int i = (k*2-2)%numbers_len;
+    return numbers[i];
+}
