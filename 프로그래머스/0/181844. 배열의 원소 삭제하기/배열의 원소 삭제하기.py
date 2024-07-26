cf12bc555c42cab7e9cdcfd365716a5a23be342f
l52w <62008219+l52w@users.noreply.github.com>
2024-07-26 21:21:28 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,8 +1,6 @@
 def solution(arr, delete_list):
     answer = []
     for i in range(len(arr)) :
-        if arr[i] in delete_list :
-            pass
-        else :
+        if arr[i] not in delete_list :
             answer.append(arr[i])
     return answer
